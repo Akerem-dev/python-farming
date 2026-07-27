3ca967542eac21700723d0e509e3d8b42d89e8ec
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-07-28 01:06:38 +0300

    design: wrap workspace with refined visual layer

---
+++ src/pages/WorkspacePage/WorkspacePageRefined.tsx
@@ -0,0 +1,10 @@
+import { WorkspacePage as WorkspacePageBase } from "./WorkspacePage";
+import "./WorkspacePageRefined.css";
+
+export function WorkspacePage() {
+  return (
+    <div className="workspace-refined-shell">
+      <WorkspacePageBase />
+    </div>
+  );
+}
