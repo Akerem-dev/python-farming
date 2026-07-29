c2ee22788bc124f94168972bbbca749fbdbcef31
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-07-29 16:20:50 +0300

    fix(ui): tighten compact projects hero

---
+++ src/pages/ProjectsPage/ProjectsPageFinal.tsx
@@ -0,0 +1,10 @@
+import { ProjectsPage as ProjectsPageBase } from "./ProjectsPageRefined";
+import "./ProjectsPageFinal.css";
+
+export function ProjectsPage() {
+  return (
+    <div className="projects-final-shell">
+      <ProjectsPageBase />
+    </div>
+  );
+}
